5b7d16c219bf1e6c8caedf826ff01659a6f01377
Bo <admin@MacBook-Pro.local>
2015-02-08 00:51:52 +0400

    + modal window in works section

--- static/menu.js
+++ static/menu.js
@@ -1,6 +1,20 @@
 $( document ).ready(function() {
-   $( ".menu" ).click(function() {
+
+
+  $( ".add" ).click(function(e) {
+  	e.preventDefault();
+	$( ".modal" ).css("display","block");
+    });
+
+   $( ".modal__close" ).click(function(e) {
+  	e.preventDefault();
+	$( ".modal" ).css("display","none");
+    });
+	
+
+
+	$( ".menu" ).click(function() {
    	console.log("works");
-$( ".header__menu-pop-up" ).toggle();
+	$( ".header__menu-pop-up" ).toggle();
 	});
 });
